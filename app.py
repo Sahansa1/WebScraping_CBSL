39611edb6b00c490c529400ce9d417f6caf3bdd4
Sahansa Amarasinghe <150770509+Sahansa1@users.noreply.github.com>
2025-09-21 13:27:27 +0000

    Added inflation page

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import plotly.express as px
 from policyRatesScraper import render_policy_rates_page
 from exchangeRatesScraper import latest_currency_rates
+from inflationScraper import render_inflation_page
 
 # Set Streamlit page configuration
 st.set_page_config(
@@ -16,7 +17,8 @@ st.set_page_config(
 st.title("Central Bank of Sri Lanka Data Dashboard")
 
 st.sidebar.title("Navigation")
-selected_page = st.sidebar.radio("Select Page", ["Home", "Policy Rates", "Exchange Rates"])
+selected_page = st.sidebar.radio(
+    "Select Page", ["Home", "Policy Rates", "Exchange Rates", "Inflation"])
 
 # --- Home Page ---
 if selected_page == "Home":
@@ -40,31 +42,38 @@ elif selected_page == "Exchange Rates":
     df = pd.read_csv(os.path.join("Data", "exchange_rates.csv"))
     df['Date'] = pd.to_datetime(df['Date'], format='%Y-%m-%d')
     df.set_index('Date', inplace=True)
-    
+
     currencies = [col[-4:].strip() for col in df.columns if col != "Date"]
     curr = st.selectbox("Select Currency", set(currencies))
-    
+
     if curr:
         latest_rates = latest_currency_rates(df, curr)
         st.subheader(f"Latest Rates for {curr} as of {latest_rates['date']}")
-        
+
         col1, col2 = st.columns(2)
         col1.metric("Buying Rate", f"Rs.{latest_rates['buying']}")
         col2.metric("Selling Rate", f"Rs.{latest_rates['selling']}")
-    
+
     st.subheader("Historical Exchange Rates")
-    
+
     tab1, tab2 = st.tabs(["Buying", "Selling"])
-    
+
     plot_data = df.reset_index()
     plot_data['Date'] = plot_data['Date'].dt.strftime('%Y-%m-%d')
 
     with tab1:
         if curr:
-            fig = px.line(plot_data, x="Date", y=f"TT Rates -Buying {curr}", title=f"{curr} Over Time")
+            fig = px.line(
+                plot_data, x="Date", y=f"TT Rates -Buying {curr}", title=f"{curr} Over Time")
             st.plotly_chart(fig, use_container_width=True)
-            
+
     with tab2:
         if curr:
-            fig = px.line(plot_data, x="Date", y=f"TT Rates -Selling {curr}", title=f"{curr} Over Time")
+            fig = px.line(
+                plot_data, x="Date", y=f"TT Rates -Selling {curr}", title=f"{curr} Over Time")
             st.plotly_chart(fig, use_container_width=True)
+
+
+# --- Inflation Page ---
+elif selected_page == "Inflation":
+    render_inflation_page()
